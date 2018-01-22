0b931251794b1476454f889354431f5a031617f2
Wirunpong Jaingamlertwong <wirunpong.j@outlook.com>
2018-01-22 16:29:10 +0700

    set ui tab view controller and set icon

---
+++ AniDesu/AniDesu/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AniDesu
+//
+//  Created by Wirunpong Jaingamlertwong on 22/1/2561 BE.
+//  Copyright © 2561 Wirunpong Jaingamlertwong. All rights reserved.
+//
+
+import Foundation
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// Segues
+let TO_HOME = "toHome"
